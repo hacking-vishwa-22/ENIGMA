ea92b88513e35642888ca6a810bbcb3924d249de
Ashish <thisisashish.02@gmail.com>
2022-04-23 03:23:47 +0530

    integrate cart in product

--- enigma/src/components/products/productItem/ProductItem.js
+++ enigma/src/components/products/productItem/ProductItem.js
@@ -1,8 +1,20 @@
+import { useContext } from 'react';
 import ProductItemForm from './ProductItemForm';
 import classes from './ProductItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const ProductItem = (props) => {
+    const cartCtx = useContext(CartContext);
     const price = `₹${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price,
+        });
+    };
 
     return (
         <li className={classes.product}>
@@ -14,7 +26,7 @@ const ProductItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div className={classes.form}>
-                <ProductItemForm id={props.id} />
+                <ProductItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
