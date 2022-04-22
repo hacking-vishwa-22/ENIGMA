ddf222c8cbfa0e49735d6c9c8979d2bbf9dc86ff
prakalpshakya31 <prakalpshakya31@gmail.com>
2022-04-22 21:58:12 +0530

    add authentication

---
+++ enigma/src/components/pages/AuthPage.js
@@ -0,0 +1,13 @@
+import AuthForm from '../Auth/AuthForm';
+import Header from '../Layout/Header';
+
+const AuthPage = () => {
+  return (
+    <div>
+      <Header />
+      <AuthForm />
+    </div>
+  );
+};
+
+export default AuthPage;
