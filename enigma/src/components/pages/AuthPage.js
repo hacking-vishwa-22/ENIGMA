e5e3f8d513eaa40d8c0aa40579f93b121a93e964
Ashish <thisisashish.02@gmail.com>
2022-04-22 22:32:41 +0530

    integrate auth in Header and add routes

--- enigma/src/components/pages/AuthPage.js
+++ enigma/src/components/pages/AuthPage.js
@@ -1,13 +1,13 @@
 import AuthForm from '../Auth/AuthForm';
-import Header from '../Layout/Header';
+import Header from '../layout/Header';
 
 const AuthPage = () => {
-  return (
-    <div>
-      <Header />
-      <AuthForm />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <AuthForm />
+        </div>
+    );
 };
 
 export default AuthPage;
--- enigma/src/components/pages/HomePage.js
+++ enigma/src/components/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../Layout/Header'
+import Header from '../layout/Header';
 
 const HomePage = () => {
     return (
