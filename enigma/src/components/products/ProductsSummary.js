c06035f40039c27241f504873241b8d19e3d21e1
Ashish <thisisashish.02@gmail.com>
2022-04-23 11:10:37 +0530

    update product summary

--- enigma/src/components/products/ProductsSummary.js
+++ enigma/src/components/products/ProductsSummary.js
@@ -3,18 +3,18 @@ import classes from './ProductsSummary.module.css';
 const ProductsSummary = () => {
     return (
         <section className={classes.summary}>
-            <h2>A platform for farms!</h2>
+            <h2>A platform for farmers and farm products!</h2>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.{' '}
+                We provide a platform for small-scale and marginalized farmers
+                to directly buy their agro-requirements from large retailers and
+                traders at relatively cheaper prices and also to sell their
+                products to consumers through our platform eliminating middlemen
+                culture and to increase their profit share.
             </p>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.{' '}
+                Small scale farmers comprise more than 70% of total farmers in
+                India and Krishi Vyapar focuses on welfare of these marginalized
+                sections.
             </p>
         </section>
     );
